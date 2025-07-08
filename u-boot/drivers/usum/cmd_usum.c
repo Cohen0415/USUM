e00f31c406df3859c9a9b6a80ebd128fb1d415e2
Cohen0415 <1033878279@qq.com>
2025-07-08 23:53:10 +0800

    update: usum: 初始化用户所选择的存储设备信息

--- u-boot/drivers/usum/cmd_usum.c
+++ u-boot/drivers/usum/cmd_usum.c
@@ -9,6 +9,7 @@
 
 #include "cmd_usum.h"
 
+static storage_configs_t cfg;
 static void read_line(char *buf, int maxlen)
 {
     int i = 0;
@@ -43,15 +44,63 @@ static void read_line(char *buf, int maxlen)
     buf[i] = '\0';
 }
 
-static void selected_storage_dev(char *dev)
+static void selected_storage_dev(char *dev, storage_configs_t *cfg)
 {
-	// 这里可以添加对选中设备的处理逻辑
+	memset(cfg, 0, sizeof(storage_configs_t));
+
+	// init stroage_type
 	printf("Selected storage device: %s\n", dev);
+	if (strcmp(dev, USUM_STORAGE_UDISK) == 0) 
+	{
+		strncpy(cfg->stroage_type, "usb", MAX_CFG_LEN - 1);
+	} 
+	else if (strcmp(dev, USUM_STORAGE_SDCARD) == 0) 
+	{
+		strncpy(cfg->stroage_type, "mmc", MAX_CFG_LEN - 1);
+	} 
+	else 
+	{
+		printf("Unknown storage device, defaulting to USB.\n");
+		strncpy(cfg->stroage_type, "usb", MAX_CFG_LEN - 1);
+	}
+
+	char inbuf[10] = {0};
+
+	// init stroage_dev_num
+	printf("Enter %s device number (default 0): ", cfg->stroage_type);
+	read_line(inbuf, sizeof(inbuf));
+	if (inbuf[0] != '\0')
+	{
+		strncpy(cfg->stroage_dev_num, inbuf, MAX_CFG_LEN - 1);
+	}
+	else 
+	{
+		printf("Using default device number 0.\n");
+		strncpy(cfg->stroage_dev_num, "0", MAX_CFG_LEN - 1);
+	}
+	
+	// init stroage_partition
+	printf("Enter %s partition (default 1): ", cfg->stroage_type);
+	read_line(inbuf, sizeof(inbuf));
+	if (inbuf[0] != '\0')
+	{
+		strncpy(cfg->stroage_partition, inbuf, MAX_CFG_LEN - 1);
+	}
+	else 
+	{
+		printf("Using default partition 1.\n");
+		strncpy(cfg->stroage_partition, "1", MAX_CFG_LEN - 1);
+	}
+
+	printf("Storage configuration:\n");
+	printf("  Type: %s\n", cfg->stroage_type);
+	printf("  Device Number: %s\n", cfg->stroage_dev_num);	
+	printf("  Partition: %s\n", cfg->stroage_partition);
 }
 
 static int do_usum(struct cmd_tbl_s *cmdtp, int flag, int argc, char *const argv[])
 {  
-	char *storage_dev[] = {"udisk", "sd card", "tftp"};
+	char *storage_dev[] = {USUM_STORAGE_UDISK, USUM_STORAGE_SDCARD};
 	int storage_dev_count = sizeof(storage_dev) / sizeof(storage_dev[0]);
 	char inbuf[16] = {0};
 
@@ -79,8 +128,17 @@ static int do_usum(struct cmd_tbl_s *cmdtp, int flag, int argc, char *const argv
 		if (selected_id < 0 || selected_id >= storage_dev_count) 
 			continue;
 
-		// 设备选择
-		selected_storage_dev(storage_dev[selected_id]);
+		// 存储介质选择
+		selected_storage_dev(storage_dev[selected_id], &cfg);
+
+		// 检查所选存储介质是否存在
+		char dev_part[10];
+		snprintf(dev_part, sizeof(dev_part), "%s:%s", cfg.stroage_dev_num, cfg.stroage_partition);
+		if (fs_set_blk_dev(cfg.stroage_type, dev_part, USUM_FS_TYPE)) 
+		{
+			printf("Failed to set block device %s %s:%s\n", cfg.stroage_type, cfg.stroage_dev_num, cfg.stroage_partition);
+			continue;
+		}
     }
 
 	return 0;
--- u-boot/drivers/usum/cmd_usum.h
+++ u-boot/drivers/usum/cmd_usum.h
@@ -1,5 +1,16 @@
 #ifndef _CMD_USUM_H
 #define _CMD_USUM_H
 
+#define USUM_STORAGE_UDISK              "udisk"
+#define USUM_STORAGE_SDCARD             "sdcard"
+
+#define USUM_FS_TYPE                   FS_TYPE_FAT     // 文件系统类型，目前支持FAT
+
+#define MAX_CFG_LEN 32      
+typedef struct storage_configs {
+    char stroage_type[MAX_CFG_LEN];  
+    char stroage_dev_num[MAX_CFG_LEN];
+    char stroage_partition[MAX_CFG_LEN];
+} storage_configs_t;
 
 #endif
